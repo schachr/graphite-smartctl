2adb5353b9f4c62f5ef8271c6554c74f0d3b3ea1
schachr <schachr@github.com>
2017-09-19 08:13:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # graphite-smartctl
+It's a simple script which parses the output of all deteced hard disks via `smartctl` and feed it into graphite.
+Only one connection is made to send all the catched metrics to graphite.
+
+Tested on:
++ debian 9.0
++ armbian
++ enigma2 (VTi)
++ cygwin
+
+## Configuration
+
+Please adjust the variables
++ `_graphiteprefix`
++ `_graphitehost`
++ `_graphiteport`
+
+... at the beginning of the file to match your environment.
+
+## Single Disk Mode
+
+Optionally you can provide a single disk via argument to gather statistics from:
+`./graphite-smartctl.sh sda`
+
+
+##
+
+Copyright (c)2017 by [schachr](https://github.com/schachr)
+
+This script comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions. See CC BY-NC-SA 4.0 for details.
+https://creativecommons.org/licenses/by-nc-sa/4.0/
---
+++ graphite-smartctl.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# please use your graphite preferences here
+_graphiteprefix="schachr"
+_graphitehost="graphitehost.example.com"
+_graphiteport="2003"
+
+# generated variables
+_device=${1:-$(for device in /dev/sd? ; do basename /dev/$device ; done)}
+_hostname=$(hostname -s | tr '[:upper:]' '[:lower:]')
+_date=$(date +%s)
+_cygwinfix=$(if ! uname -a | egrep -q "(Cygwin| mips )" ; then echo -n "-q0" ; fi)
+smartctlcmd=$(which smartctl 2>/dev/null)
+declare -a _output
+
+# sanity check device
+if [[ -z ${_device} ]] ; then
+    echo "No device set or found."
+    exit 1
+fi
+
+# sanity check smartctl
+if   [[ -z $smartctlcmd && -f /usr/sbin/smartctl ]] ; then
+    smartctlcmd="/usr/sbin/smartctl"
+elif [[ -z $smartctlcmd ]] ; then
+    echo "smartctl not found."
+    exit 1
+fi
+
+# sanity check smartctl output and get first usable header
+for device in $_device ; do
+    header=$($smartctlcmd -a /dev/$device | awk "/^ID#/ {printline = 1; print; next} /^$/ {printline = 0} printline" | head -n1)
+    [[ ! -z $header ]] && break
+done
+
+# sanity check header
+if [[ -z $header ]] ; then
+    echo "A header in smartctl for device values could not be found."
+    exit 1
+fi
+
+# process output of smartctl
+for device in $_device ; do
+    while IFS= read -r line ; do
+        prefix=$(echo $line | awk '{print $1"."$2}')
+
+        # dynamic fields - slower
+        #for i in $( seq 2 $(awk '{print NF}' <<< $line) ) ; do
+        # static fields - faster
+        for i in 4 5 6 10 ; do 
+            value=$(echo $line | cut -f$i -d" ")
+
+            if [[ $value =~ ^-?[0-9]+([.][0-9]+)?$ ]] ; then
+                _output+=("$_graphiteprefix.$_hostname.disk.$device.smart.$prefix.$(echo $header | cut -f$i -d" ") $value "$_date)
+                [ -t 1 ] && echo ${_output[-1]}
+            fi
+
+        done
+    done < <($smartctlcmd -a /dev/$device | awk "/^ID#/ {printline = 1; print; next} /^$/ {printline = 0} printline" | tail -n+2)
+done
+
+# send to graphite
+[[ ! $(echo ${#_output[@]}) -eq 0 ]] && printf '%s\n' "${_output[@]}" | nc $_cygwinfix $_graphitehost $_graphiteport > /dev/null 2>&1
